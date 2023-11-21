d8b04bcfdd9a63eb7aa3b933a30f4075266b6724
Karoline Ferreira <karoline.a.ferreira@hotmail.com>
2023-11-21 16:00:51 -0300

    View for New/Create

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :coworking, only: %i[index new create]
+  resources :coworkings, only: %i[index new create]
 end
