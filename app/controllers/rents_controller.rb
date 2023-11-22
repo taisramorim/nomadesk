273e4216faa03f7ea508cff6127afa07b9cf14e2
stella mascaro <stellamascaro@hotmail.com>
2023-11-22 16:08:10 -0300

    book button created - new/create

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -1,3 +1,32 @@
 class RentsController < ApplicationController
+  before_action :set_coworking, only: [:new, :create]
 
+  def index
+    @rents = Rent.all
+  end
+
+  def new
+    @rent = Rent.new
+  end
+
+  def create
+    @rent = Rent.new(rent_params)
+    @rent.coworking = @coworking
+    @rent.user = current_user
+    if @rent.save
+      redirect_to root_path, notice: 'Rent was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_coworking
+    @coworking = Coworking.find(params[:coworking_id])
+  end
+
+  def rent_params
+    params.require(:rent).permit(:start_date, :end_date, :coworking_id, :user_id)
+  end
 end
